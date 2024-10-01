f10768e48e9700bb4f201fd6e125f2468410bfed
Omimacgithub <omar.montenegrom@udc.es>
2024-10-01 20:00:14 +0200

    Failed approach of my_dense

--- my_dense.c
+++ my_dense.c
@@ -1,6 +1,21 @@
 #include "spmv.h"
+#include <math.h>
 
 int my_dense(const unsigned int n, const double mat[], double vec[], double result[])
 {
   // code your own solver
+  int rows=0, j=0, z=0;
+  double tmp = 0.0;
+  //Assuming an NxN matrix 
+  rows = (int) sqrt(n);
+  while(rows <= n ){
+  	for (j=j; j < rows ; ++j){
+		tmp = tmp + mat[j] * vec[j];
+
+	}
+	result[z] = tmp;
+	tmp=0;
+	++z;
+	rows += j;
+  }
 }
